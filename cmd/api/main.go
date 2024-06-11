9e48887686268f833ee1da161ab3b635a6722fde
vishnusunil2 <vishnusunil-ext@superleap.com>
2024-06-11 16:14:37 +0530

    added printroutes for printing routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
 	"os"
 	v1 "team-management/cmd/api/handlers/v1"
 	"team-management/common/config"
 	"team-management/common/database"
-	"team-management/common/logger"
 	"team-management/di"
 	"team-management/internal/models"
 )
@@ -21,7 +21,6 @@ func main() {
 		logrus.Fatalf("failed to load configurations")
 		os.Exit(1)
 	}
-	logger.Init(cfg.LogLevel)
 	db, err := database.Init(cfg)
 	if err != nil {
 		logrus.Fatalf("failed to initialise the datbase")
@@ -35,6 +34,11 @@ func main() {
 	e := echo.New()
 	apiGroup := e.Group("/api/v1")
 	v1.RegisterHandlers(apiGroup, deps)
-
+	printRoutes(e)
 	e.Logger.Fatal(e.Start(cfg.ServerAddress))
+}
+func printRoutes(e *echo.Echo) {
+	for _, route := range e.Routes() {
+		fmt.Printf("%s %s\n", route.Method, route.Path)
+	}
 }
--- internal/service/user_service/request.go
+++
@@ -1,9 +0,0 @@
-package user_service
-
-type UserSignupRequest struct {
-	Email     string `json:"email"`
-	Phone     string `json:"phone"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Password  string `json:"password"`
-}
